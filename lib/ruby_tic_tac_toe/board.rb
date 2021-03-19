6852c0912091867566b5ca8525fbbf3f980f4dd0
Kelly Yong <kelly.a.yong@gmail.com>
2021-03-19 18:50:43 -0400

    update board and spec

--- lib/ruby_tic_tac_toe/board.rb
+++ lib/ruby_tic_tac_toe/board.rb
@@ -23,5 +23,34 @@ module TicTacToe
             return :draw if draw?
             false
         end
+
+        def draw?
+            grid.flatten.map { |cell| cell.value }.none_empty?
+        end
+
+        def winning_positions
+            grid +
+            grid.transpose +
+            diagonals
+        end
+        
+        def diagonals 
+            [
+                [get_cell(0,0), get_cell(1,1), get_cell(2,2)],
+                [get_cell(0,2), get_cell(1,1), get_cell(2,0)]
+            ]
+        end
+
+        def winner?
+            winning_positions.each do |winning_position|
+                next if winning_position_values(winning_position).all_empty?
+                return true if winning_position_values(winning_position).all_same?
+            end
+            false
+        end
+
+        def winning_position_values(winning_position)
+            winning_position.map { |cell| cell.value }
+
     end
 end
